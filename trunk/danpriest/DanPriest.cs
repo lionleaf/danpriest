b14c4320a06cdcd564056a7dbae9e37ee39db704
gwethir <gwethir@2ad962bc-774a-0410-b8ba-fd541a9e4636>
2008-05-09 19:06:55 +0000

    Note to self: Always try to compile before commiting. Fixed a syntax error

--- trunk/danpriest/DanPriest.cs
+++ trunk/danpriest/DanPriest.cs
@@ -1639,7 +1639,7 @@ namespace Glider.Common.Objects
                 return true;
             }
             Refresh();
-            if (Me.Health <= Simple_Renew && IsKeyEnabled("DP.Renew") && !HasBuff("Renew"))
+            if (Me.Health <= Simple_Renew && IsKeyEnabled("DP.Renew") && !HasBuff("Renew") && !IsShadowform())
             {
                 CastSpell("DP.Renew");
                 return true;
@@ -3111,6 +3111,7 @@ namespace Glider.Common.Objects
     partial class DanPriest
     {
         //Add PvP variables here!
+        bool PvP_UseMindFlay, PvP_UseVampiricTouch = true;
 
     }
 }
